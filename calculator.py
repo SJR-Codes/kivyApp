c0b257850838ad805a6a3bf071fe32fabe66d9aa
Samu Reinikainen <samu.reinikainen@gmail.com>
2022-12-05 13:01:55 +0200

    rc 0.2

--- calculator.py
+++ calculator.py
@@ -6,40 +6,65 @@ class CalcWidget(Widget):
         super().__init__()
         #allow only one operand
         self.operandUsed = False
+        self.separatorUsed = False
+        self.precision = 0
 
     #laskimen toiminnot tänne
     def btn_exit(self):
         exit()
 
     def btn_num(self, value):
+        if self.separatorUsed:
+            self.precision += 1
         if self.ids.calc_input.text == "0" or "=" in self.ids.calc_input.text:
             self.ids.calc_input.text = str(value)
         else:
             self.ids.calc_input.text += str(value)
 
+    #insert separator
+    #TODO: , or . based on locale
     def btn_dot(self):
+        self.separatorUsed = True
+        #only one dot at time
         if self.ids.calc_input.text[-1] != ".":
             self.ids.calc_input.text += "."
 
+    #insert operand for calculation
     def btn_operand(self, value):
         if not self.operandUsed:
             self.ids.calc_input.text += str(value)
             self.operandUsed = True
 
+    #clear input
     def btn_c(self):
+        #reset magic
+        self.operandUsed = False
+        self.separatorUsed = False
+        self.precision = 0
+        #reset input field
         self.ids.calc_input.text = ""
 
+    #delete last added char
+    def btn_ce(self):
+        self.ids.calc_input.text = self.ids.calc_input.text[:-1]
+
+    #negate number
     def btn_negate(self):        
         if not self.operandUsed:
             self.ids.calc_input.text = str(int(self.ids.calc_input.text)*-1)
 
     def btn_calculate(self):
-        self.operandUsed = False
         try:
-            res = eval(self.ids.calc_input.text)
+            res = round(eval(self.ids.calc_input.text), self.precision)
             self.ids.calc_input.text += " = " + str(res)
         except SyntaxError:
             self.ids.calc_input.text = "err"
+        except NameError:
+            self.ids.calc_input.text = "errkele!"
+        #reset magic
+        self.operandUsed = False
+        self.separatorUsed = False
+        self.precision = 0
     
 
 #main class same ass apps filename
